830b604be44d2c8d17f5379912204619c70c88f0
HANNOUCHY <hannouchy@gmail.com>
2020-11-22 22:59:27 +0100

    menu mobile desktop avec les services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationDesktopComponent } from './navigation-desktop/navigation-desktop.component';
+import { NavigationMobileFooterComponent } from './navigation-mobile-footer/navigation-mobile-footer.component';
+import { NavigationMobileHeaderComponent } from './navigation-mobile-header/navigation-mobile-header.component';
 import {dataconfig} from './services/configmenu';
 @Component({
   selector: 'app-root',
@@ -6,6 +9,9 @@ import {dataconfig} from './services/configmenu';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  navigationMobileHeaderComponent = NavigationMobileHeaderComponent;
+  navigationMobileFooterComponent = NavigationMobileFooterComponent;
+  navigationDesktopComponent = NavigationDesktopComponent;
 config = dataconfig;
 constructor(){
 }
