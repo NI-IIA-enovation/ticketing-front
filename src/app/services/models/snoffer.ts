e2cf270bb27940dfb3284137b68faaba19def5b5
HANNOUCHY <hannouchy@gmail.com>
2021-01-15 22:31:32 +0100

    Nettoyage code

--- src/app/services/models/snoffer.ts
+++ src/app/services/models/snoffer.ts
@@ -8,16 +8,16 @@ export class SNOffer extends SNItem {
     private $addres: string;
     private $editmode: string;
     private $country: string;
-   /* private $addresse_number: string;
-    private $address_publish: string;*/
+    private $addresse_number: string;
+    private $address_publish: string;
     private $bought: string;
-   /* private $building_faces: string;
+    private $building_faces: string;
     private $building_groundarea: string;
     private $building_levels: string;
     private $cache_epclabel: string;
-    private $cache_regionlevel1: string;*/
+    private $cache_regionlevel1: string;
     private $city: string;
-   /* private $desc_simple: string;
+   private $desc_simple: string;
     private $finance_price: string;
     private $house_bednumber: string;
     private $house_category: string;
@@ -25,7 +25,7 @@ export class SNOffer extends SNItem {
     private $house_sdbnumber: string;
     private $house_suphabi: string;
     private $house_supjardin: string;
-    private $house_supsol: string;*/
+    private $house_supsol: string;
     private $reference: string;
     private $status: string;
     private $sticker: string;
@@ -50,78 +50,78 @@ export class SNOffer extends SNItem {
         this.$country = this.getProperty('country');
         return this.getProperty('country');
     }
-   /* get addresse_number(): string {
-        this.$addresse_number=this.getProperty('addresse_number');
+   get addresse_number(): string {
+        this.$addresse_number = this.getProperty('addresse_number');
         return this.getProperty('addresse_number');
     }
     get address_publish(): string {
-        this.$address_publish=this.getProperty('address_publish');
+        this.$address_publish = this.getProperty('address_publish');
         return this.getProperty('address_publish');
-    }*/
+    }
     get bought(): string {
         this.$bought = this.getProperty('bought');
         return this.getProperty('bought');
     }
-   /* get building_faces(): string {
-        this.$building_faces=this.getProperty('building_faces');
+   get building_faces(): string {
+        this.$building_faces = this.getProperty('building_faces');
         return this.getProperty('building_faces');
     }
     get building_groundarea(): string {
-        this.$building_groundarea=this.getProperty('building_groundarea');
+        this.$building_groundarea = this.getProperty('building_groundarea');
         return this.getProperty('building_groundarea');
     }
     get building_levels(): string {
-        this.$building_levels=this.getProperty('building_levels');
+        this.$building_levels = this.getProperty('building_levels');
         return this.getProperty('building_levels');
     }
     get cache_epclabel(): string {
-        this.$cache_epclabel=this.getProperty('cache_epclabel');
+        this.$cache_epclabel = this.getProperty('cache_epclabel');
         return this.getProperty('cache_epclabel');
     }
     get cache_regionlevel1(): string {
         this.$cache_regionlevel1 = this.getProperty('cache_regionlevel1');
         return this.getProperty('cache_regionlevel1');
-    }*/
+    }
     get city(): string {
         this.$city = this.getProperty('city');
         return this.getProperty('city');
     }
-   /* get desc_simple(): string {
-        this.$desc_simple=this.getProperty('desc_simple');
+   get desc_simple(): string {
+        this.$desc_simple = this.getProperty('desc_simple');
         return this.getProperty('desc_simple');
     }
     get finance_price(): string {
-        this.$finance_price=this.getProperty('finance_price');
+        this.$finance_price = this.getProperty('finance_price');
         return this.getProperty('finance_price');
     }
     get house_bednumber(): string {
-        this.$house_bednumber=this.getProperty('house_bednumber');
+        this.$house_bednumber = this.getProperty('house_bednumber');
         return this.getProperty('house_bednumber');
     }
     get house_category(): string {
-        this.$house_category=this.getProperty('house_category');
+        this.$house_category = this.getProperty('house_category');
         return this.getProperty('house_category');
     }
     get house_garages(): string {
-        this.$house_garages=this.getProperty('house_garages');
+        this.$house_garages = this.getProperty('house_garages');
         return this.getProperty('house_garages');
     }
     get house_sdbnumber(): string {
-        this.$house_sdbnumber=this.getProperty('house_sdbnumber');
+        this.$house_sdbnumber = this.getProperty('house_sdbnumber');
         return this.getProperty('house_sdbnumber');
     }
     get house_suphabi(): string{
-        this.$house_suphabi=this.getProperty('house_suphabi');
+        this.$house_suphabi = this.getProperty('house_suphabi');
         return this.getProperty('house_suphabi');
     }
     get house_supjardin(): string {
-        this.$house_supjardin=this.getProperty('house_supjardin');
+        this.$house_supjardin = this.getProperty('house_supjardin');
         return this.getProperty('house_supjardin');
     }
     get house_supsol(): string {
         this.$house_supsol = this.getProperty('house_supsol');
         return this.getProperty('house_supsol');
-    }*/
+    }
     get reference(): string {
         this.$reference =  this.getProperty('reference');
         return this.getProperty('reference');
--- src/app/services/offer.service.ts
+++ src/app/services/offer.service.ts
@@ -3,6 +3,7 @@ import { SNOffer } from './models/snoffer';
 import { ODBService } from 'projects/snitem/src/lib/apisnitem/odb.service';
 import { Observable } from 'rxjs';
 import { shareReplay, tap } from 'rxjs/operators';
+import { SNItemFactory } from 'projects/snitem/src/lib/apisnitem/models/snitemfactory';
 
 
 
@@ -11,8 +12,9 @@ import { shareReplay, tap } from 'rxjs/operators';
 })
 export class OfferService {
   data$: Observable<SNOffer>;
-  constructor(private serviceodb: ODBService) {
-    this.data$ =  this.serviceodb.call('/fr/9383/offer/search', [['oquery', 'SNItem.SNOffer(publish%3DYes%26bought%3DNo%26type%3DVente)$SNItem.SNOffer(finance_price+%23)']], '', 'get', 'items').pipe(
+  constructor(private serviceodb: ODBService,private factory: SNItemFactory ) {
+    factory.addItem('SNItem.SNOffer', new SNOffer());
+    this.data$ =  this.serviceodb.call('/fr/9383/offer/search', [['oquery', 'SNItem.SNOffer(publish%3DYes%26bought%3DNo%26type%3DVente)$SNItem.SNOffer(finance_price+%23)']], '', 'get', 'items' , 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6ImZrNWxmNTRicG81NTlwczQ2cHZzMjMwMjI2In0.eyJpc3MiOiJodHRwOlwvXC9zd2lmdGltbW8uZXUiLCJhdWQiOiJodHRwOlwvXC93d3cucGVyaXNvZnQuYmVcL3N3aWZ0aW1tbyIsInN1YiI6IndlYnNpdGUiLCJqdGkiOiJmazVsZjU0YnBvNTU5cHM0NnB2czIzMDIyNiIsImlhdCI6MTYwOTkyNzE1OSwibmJmIjoxNjA5OTI3MTU5LCJleHAiOjE2MDk5MjkzOTIsInJvbGVzIjoiW3tcIm5hbWVcIjpcIndlYnNpdGVcIn1dIiwiZG9tYWluaWQiOjkzODMsInVzZXJpZCI6IjkyNjgxNCJ9.A1Pl19zvgGZg3AD87kF5n-HTV3tV6PS8prn9-Hr_TCk').pipe(
       tap(console.log),
       shareReplay(1),
       tap(() => console.log('after sharing'))
