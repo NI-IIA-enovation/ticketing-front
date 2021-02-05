3b649735080beff2e9fe7cfc4dc97ef806b60e05
yassinassecoum <yassin.assecoum@hotmail.com>
2021-02-05 10:14:25 +0100

    sncontact

---
+++ src/app/services/models/sncontact.ts
@@ -0,0 +1,19 @@
+
+export class SNcontact {
+    private key: string;
+    private name: string;
+    private email: string;
+    private gsm: string;
+    private notif: number;
+    private visit: number;
+    private surface: number;
+    private room: number;
+    private bathroom: number;
+    private status: string;
+    private type: string;
+    constructor() {
+        this.key = 'SNItem.SNContact';
+    }
+
+
+}
